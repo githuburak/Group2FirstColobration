8a9d9749f23b61528851e121a63d2c2e432e94ac
OnurHarmandar <harmandaronur@gmail.com>
2021-12-27 10:41:42 +0300

    Second work from onur

--- src/test/java/com/cydeo/test/Test_Onur.java
+++ src/test/java/com/cydeo/test/Test_Onur.java
@@ -3,5 +3,7 @@ package com.cydeo.test;
 public class Test_Onur {
     public static void main(String[] args) {
         System.out.println("First work from onur");
+
+        System.out.println("Second work from onur");
     }
 }
