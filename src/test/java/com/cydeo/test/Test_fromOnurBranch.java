256ca2d7d7a529d3542f43121ca78ef6164910f6
Benjamin2955 <ozenbunyamin29@gmail.com>
2021-12-27 02:21:40 -0600

    Benjamin branch first commit

--- src/test/java/com/cydeo/test/Test_fromOnurBranch.java
+++ src/test/java/com/cydeo/test/Test_fromOnurBranch.java
@@ -2,6 +2,7 @@ package com.cydeo.test;
 
 public class Test_fromOnurBranch {
     public static void main(String[] args) {
+
         System.out.println("Test From Onur Branch");
     }
 }
---
+++ src/test/java/com/cydeo/test/test_benjamin.java
@@ -0,0 +1,7 @@
+package com.cydeo.test;
+
+public class test_benjamin {
+    public static void main(String[] args) {
+        System.out.println("First change from Benjamin");
+    }
+}
